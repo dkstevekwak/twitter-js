f4bbb055dc466ed65c0161412c4eb23bc56de2b2
Steve Kwak <steveguac@gmail.com>
2015-03-11 19:25:00 -0400

    right after completing tweet by nunber

--- tweetBank.js
+++ tweetBank.js
@@ -1,9 +1,10 @@
 var _ = require('underscore');
 
-var data = [];
+var data = [{name: 'steve', text: "hi", id: '0'}];
 
 var add = function (name, text) {
-  data.push({ name: name, text: text });
+  data.push({ name: name, text: text, id: (data.length).toString()});
+
 };
 
 var list = function () {
