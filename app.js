62fa5fbee279901d869f6e7aa8a4899883256f23
Steve Kwak <steveguac@gmail.com>
2015-03-11 16:30:18 -0400

    data storage

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 var http = require('http');
 var express = require( 'express' );
 var morgan = require('morgan');
+var swig = require('swig');
+
+// var data = require('./tweetBank');
 
 var app = express();
 var server = http.createServer();
 
+var people = [ {name: 'Full'}, {name: 'Stacker'}, {name: 'Son'} ];
+
+
+
 server.on('request', app);
-
-
 
 app.use(morgan('dev'));
 
-app.get('/', function(req, res){
-	res.send("h12");
-});
+app.engine('html', require('swig').renderFile);
 
+app.set('view engine', 'html');
+app.set('views', __dirname + "/views");
+
+//For Development -- Remove for production
+swig.setDefaults({ cache: false });
+
+app.get('/', function(req, res){
+	res.render( 'index', {title: 'Hall of Fame', people: people} );
+});
 
 
 
